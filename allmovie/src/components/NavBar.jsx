5a923fc750d4a4ed07955059b5ab2b68d5da0e8d
LucasBatista37 <lucas.batista9734@email.com>
2024-05-13 23:40:31 -0300

    primeira parte finalizada

--- allmovie/src/components/NavBar.jsx
+++ allmovie/src/components/NavBar.jsx
@@ -1,21 +1,42 @@
-import { Link } from "react-router-dom";
-import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
-import "./Navbar.css"
+import { useState } from "react";
+
+import "./Navbar.css";
 
 const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    if (!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
+
   return (
     <div>
       <nav id="navbar">
         <h2>
           <Link to="/">
-            <BiCameraMovie/>AllMovies</Link>
+            <BiCameraMovie />
+            AllMovies
+          </Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque por um filme..." />
-            <button type="submit">
-                <BiSearchAlt2/>
-            </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busque por um filme..."
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
         </form>
       </nav>
     </div>
